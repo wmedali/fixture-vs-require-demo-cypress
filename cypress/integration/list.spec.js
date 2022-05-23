15e28bc5c129aad9c416392fad2d63c95355af79
Alee <monarc1937@gmail.com>
2022-05-23 11:13:14 +0200

    implement select on dropdown

--- cypress/integration/list.spec.js
+++ cypress/integration/list.spec.js
@@ -1,8 +1,11 @@
-it.skip("try", () => {
+it("should select a contact reason", () => {
+  cy.intercept("**/getCommuneListe?zipOrTown=33000").as("searchZipCode");
   cy.visit("https://www.toutsurmoneau.fr/service-client");
   cy.get("#input-town").type("33000");
-  cy.wait(1000);
+  cy.wait("@searchZipCode");
   cy.get(".capaddress-item").first().click();
   cy.get("#sc-subject").find(".sz-select__control").click();
-  cy.contains("Travaux").click({ force: true });
+  cy.get(".sz-select__menu").should("be.visible").find(".mb-0").eq(3).click();
+  // Using Text : cy.get(".sz-select__menu").should("be.visible").contains("Travaux").click();
+  // cy.contains("Travaux").click({ force: true });
 });
